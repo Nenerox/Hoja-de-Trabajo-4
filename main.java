4b9d2a5a2dfb78510c30576609b47496df7d3378
Nenerox <andres.ps0308@gmail.com>
2026-02-11 15:59:36 -0600

    test simple de traductor

--- main.java
+++ main.java
@@ -1,7 +1,14 @@
-public class main {
-    String tipoPila;
-    PilaFactory factory = new PilaFactory();
 
-    PilaInterface<Double> pila = factory.createPila(tipoPila);
-    Calculadora calculadora = new Calculadora(pila);
+import java.util.ArrayList;
+
+public class main {
+    public static void main(String[] args) {
+        Traductor traductor = new Traductor();
+
+        String datos = "(10+20)*9";
+        
+        ArrayList<String> Postif = traductor.getPostFix(datos);
+
+        System.out.println(Postif);
+    }
 }

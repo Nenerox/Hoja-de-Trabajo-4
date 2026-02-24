493f8ed472d388bef6487407606bac2605387c78
miguel13sg <saj252149@uvg.edu.gt>
2026-02-24 12:09:41 -0600

    se arreglo el traductor para que soporte numeros con mas de 1 dígito

--- Traductor.java
+++ Traductor.java
@@ -1,10 +1,9 @@
-
 import java.util.ArrayList;
 
 public class Traductor {
 
-    PilaArrayList<String> pila = new PilaArrayList<>();
-   
+    private PilaArrayList<String> pila = new PilaArrayList<>();
+
     public int precedence(char operator) {
         return switch (operator) {
             case '^' -> 3;
@@ -13,27 +12,43 @@ public class Traductor {
             default -> 0;
         };
     }
-    
+
     public ArrayList<String> getPostFix(String infix) {
+
         ArrayList<String> output = new ArrayList<>();
-    
+        pila = new PilaArrayList<>(); //reinicia pila
+
         for (int i = 0; i < infix.length(); i++) {
             char c = infix.charAt(i);
+            if (Character.isWhitespace(c)) {
+                continue;
+            }
+
             if (Character.isDigit(c)) {
-                output.add(String.valueOf(c));
-            } else if (c == '(') {
+                StringBuilder numero = new StringBuilder();
+                while (i < infix.length() && Character.isDigit(infix.charAt(i))) {
+                    numero.append(infix.charAt(i));
+                    i++;
+                }
+                i--;
+                output.add(numero.toString());
+            }
+            else if (c == '(') {
                 pila.push(String.valueOf(c));
-            } else if (c == '^') {
-                pila.push(String.valueOf(c));
-            } else if (c == ')') {
+            }
+
+            else if (c == ')') {
                 while (!pila.isEmpty() && !pila.peek().equals("(")) {
                     output.add(pila.pop());
                 }
-                pila.pop(); // Eliminar el "("
-            } else {
-                while (!pila.isEmpty() && ((c == '^' && precedence(c) < precedence(pila.peek().charAt(0))) || (c != '^' && precedence(c) <= precedence(pila.peek().charAt(0)))))
-                {
-                    output.add(pila.pop());
+                pila.pop();
+            }
+
+            else {
+                while (!pila.isEmpty() &&
+                       ((c == '^' && precedence(c) < precedence(pila.peek().charAt(0))) ||
+                        (c != '^' && precedence(c) <= precedence(pila.peek().charAt(0))))) {
+                            output.add(pila.pop());
                 }
                 pila.push(String.valueOf(c));
             }
@@ -41,6 +56,7 @@ public class Traductor {
         while (!pila.isEmpty()) {
             output.add(pila.pop());
         }
+
         return output;
     }
 }

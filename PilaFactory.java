969b76b7ab5d22d1235c1425b9595cf451ff0ba2
Nenerox <andres.ps0308@gmail.com>
2026-02-23 18:24:33 -0600

    Se agrego caso para linked and doublelinked

--- PilaFactory.java
+++ PilaFactory.java
@@ -1,11 +1,17 @@
 public class PilaFactory {
     public PilaInterface<Double> createPila(String tipo) {
+        if (tipo == null) {
+            throw new IllegalArgumentException("Tipo de pila no puede ser null");
+        }
         switch (tipo.toLowerCase()) {
             case "vector":
                 return new PilaVector<>();
             case "arraylist":
                 return new PilaArrayList<>();
-            //agregar case lista
+            case "simple":
+                return new PilaLista<>(new LinkedList<>());
+            case "doble":
+                return new PilaLista<>(new DoubleLinkedList<>());
             default:
                 throw new IllegalArgumentException("Tipo de pila desconocido: " + tipo);
         }
